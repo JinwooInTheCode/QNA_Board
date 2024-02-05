fd9170ded99d1859a63c0180dc0faa3e66880d8f
jssa428428 <jinwoo2328@gmail.com>
2024-02-05 12:51:17 +0900

    9. OAuth2 로그인 구현

---
+++ src/main/java/com/example/oauthsession/entity/UserEntity.java
@@ -0,0 +1,20 @@
+package com.example.oauthsession.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String email;
+    private String role;
+}
